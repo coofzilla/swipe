9374ce83a6b193458abe0b75ea6f37bb883fa121
coofzilla <jeric614@yahoo.com>
2022-02-06 21:24:48 +0900

    animated ball

--- src/Ball.tsx
+++ src/Ball.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { View, Animated, Text, StyleSheet } from "react-native";
-interface BallProps {}
+import React, { useEffect } from "react";
+import { View, Animated, StyleSheet } from "react-native";
 
-const Ball: React.FC<BallProps> = () => {
-  return <View style={styles.ball} />;
+//<{}> no props
+const Ball: React.FC<{}> = () => {
+  const position = new Animated.ValueXY({ x: 0, y: 0 });
+  useEffect(() => {
+    Animated.spring(position, {
+      toValue: { x: 200, y: 500 },
+      useNativeDriver: false,
+    }).start();
+  }, []);
+
+  return (
+    <Animated.View style={position.getLayout()}>
+      <View style={styles.ball} />
+    </Animated.View>
+  );
 };
 
 export default Ball;
